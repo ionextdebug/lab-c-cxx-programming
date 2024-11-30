b54f837d660bdb00a712bb6a265da536c220c3d2
ionextdebug <focusmpublic@gmail.com>
2024-11-30 14:07:33 -0400

    feat: hash table

--- data_structures/hash_table.c
+++ data_structures/hash_table.c
@@ -28,7 +28,7 @@ unsigned int hash(char *name)
     return hash_value;
 }
 
-bool init_hash_table()
+void init_hash_table()
 {
     for (int i = 0; i < TABLE_SIZE; i++)
     {
@@ -36,14 +36,92 @@ bool init_hash_table()
     }
 }
 
-int main(void)
+void print_table()
+{
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        if (hash_table[i] == NULL)
+        {
+            printf("\t%i\t---\n", i);
+        }
+        else
+        {
+            printf("\t%i\t%s\n", i, hash_table[i]->name);
+        }
+    }
+}
+
+bool hash_table_insert(person *p)
 {
 
-    printf("NameA => %u\n", hash("NameA"));
-    printf("NameB => %u\n", hash("NameB"));
-    printf("NameC => %u\n", hash("NameC"));
-    printf("NameD => %u\n", hash("NameD"));
-    printf("NameE => %u\n", hash("NameE"));
+    if (p == NULL)
+        return false;
+
+    int index = hash(p->name);
+
+    if (hash_table[index] != NULL)
+        return false;
+
+    hash_table[index] = p;
+
+    return true;
+}
+
+person *hash_table_lookup(char *name)
+{
+    int index = hash(name);
+    if (hash_table[index] != NULL && strncmp(hash_table[index]->name, name, TABLE_SIZE) == 0)
+    {
+        return hash_table[index];
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+person *hash_table_delete(char *name)
+{
+    int index = hash(name);
+    if (hash_table[index] != NULL && strncmp(hash_table[index]->name, name, TABLE_SIZE) == 0)
+    {
+        person *tmp = hash_table[index];
+        hash_table[index] = NULL;
+        return tmp;
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+int main(void)
+{
+    person *lookup_result;
+    person jacob = {.name = "jacob", .age = 20};
+    person kate = {.name = "kate", .age = 30};
+    person phill = {.name = "phill", .age = 40};
+
+    init_hash_table();
+
+    hash_table_insert(&jacob);
+    hash_table_insert(&kate);
+    hash_table_insert(&phill);
+
+    print_table();
+
+    lookup_result = hash_table_delete("jacob");
+
+    if (lookup_result != NULL)
+    {
+        printf("Found!\n");
+    }
+    else
+    {
+        printf("Not found!\n");
+    }
+
+    print_table();
 
     return 0;
 }
